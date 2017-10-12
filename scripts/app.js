bd740588590e06c338fdc5d881ce0fff6bc02777
Rob W <robertzwinchell@gmail.com>
2017-10-12 12:10:11 -0600

    v2

--- scripts/app.js
+++ scripts/app.js
@@ -11,7 +11,124 @@ define(['jquery'], function($) {
         }
     };
 	
-    //var myClick = function(){};
+    allDeleted = 0;
+
+    $('#btnHint').click(function(){
+    	var firstX,firstY,lastX,lastY;
+    	var hints = [];
+    	for (var i = 1; i < 6; i++) {
+    		for (var j = 1; j < 6; j++) {
+    			var curIndex = Math.floor(field[i][j]/10);
+    			var xAIndex = Math.floor(field[i+1][j]/10);
+    			var yAIndex = Math.floor(field[i][j+1]/10);
+    			if(curIndex == xAIndex)
+    			{
+    				if(curIndex == Math.floor(field[i+2][j+1]/10))
+    				{
+    					firstX = i+2;
+    					firstY = j+1;
+    					lastX = i+2;
+    					lastY = j;
+    					var arr = []; arr.push(firstX,firstY,lastX,lastY);
+    					hints.push(arr);
+    				}
+    				else if(curIndex == Math.floor(field[i+2][j-1]/10))
+    				{
+    					firstX = i+2;
+    					firstY = j-1;
+    					lastX = i+2;
+    					lastY = j;
+    					var arr = []; arr.push(firstX,firstY,lastX,lastY);
+    					hints.push(arr);
+    				}
+    				else if(curIndex == Math.floor(field[i-1][j+1]/10))
+    				{
+    					firstX = i-1;
+    					firstY = j+1;
+    					lastX = i-1;
+    					lastY = j;
+    					var arr = []; arr.push(firstX,firstY,lastX,lastY);
+    					hints.push(arr);
+    				}
+    				else if(curIndex == Math.floor(field[i-1][j-1]/10))
+    				{
+    					firstX = i-1;
+    					firstY = j-1;
+    					lastX = i-1;
+    					lastY = j;
+    					var arr = []; arr.push(firstX,firstY,lastX,lastY);
+    					hints.push(arr);
+    				}
+    			}
+    			else if(curIndex == yAIndex)
+    			{
+    				if(curIndex == Math.floor(field[i+1][j+2]/10))
+    				{
+    					firstX = i+1;
+    					firstY = j+2;
+    					lastX = i;
+    					lastY = j+2;
+    					var arr = []; arr.push(firstX,firstY,lastX,lastY);
+    					hints.push(arr);
+    				}
+    				else if(curIndex == Math.floor(field[i-1][j+2]/10))
+    				{
+    					firstX = i-1;
+    					firstY = j+2;
+    					lastX = i;
+    					lastY = j+2;
+    					var arr = []; arr.push(firstX,firstY,lastX,lastY);
+    					hints.push(arr);
+    				}
+    				if(curIndex == Math.floor(field[i+1][j-1]/10))
+    				{
+    					firstX = i+1;
+    					firstY = j-1;
+    					lastX = i;
+    					lastY = j-1;
+    					var arr = []; arr.push(firstX,firstY,lastX,lastY);
+    					hints.push(arr);
+    				}
+    				else if(curIndex == Math.floor(field[i-1][j-1]/10))
+    				{
+    					firstX = i-1;
+    					firstY = j-1;
+    					lastX = i;
+    					lastY = j-1;
+    					var arr = []; arr.push(firstX,firstY,lastX,lastY);
+    					hints.push(arr);
+    				}
+    			}
+     		}
+    	}
+    	console.log(hints);
+    	if(hints.length>0)
+    	{
+    		x1 = hints[0][0];
+    		y1 = hints[0][1];
+    		x2 = hints[0][2];
+    		y2 = hints[0][3];
+    		console.log(x1,y1,x2,y2);
+    		for(var i = 0 ; i < 3 ; i++){
+    			$('#xy' + x1 + y1).animate({ opacity:0.2 }, 500);
+	    		$('#xy' + x1 + y1).animate({ opacity:1 }, 500);
+	    		$('#xy' + x2 + y2).animate({ opacity:0.2 }, 500);
+	    		$('#xy' + x2 + y2).animate({ opacity:1 }, 500);
+    		}
+    	}
+    });
+
+    $('#btnShuttle').click(function(){
+    	$('div').remove('.item');
+	    for (var x = 0; x < 8; x++) {
+            field[x] = new Array(8);
+            for (var y = 0; y < 8; y++) {
+                field[x][y] = Math.ceil(Math.random() * 7)*10;
+                self._addPiece(x, y, field[x][y]);
+            }
+        }
+    	self._processField();
+    });
 
     return {
 
@@ -27,14 +144,13 @@ define(['jquery'], function($) {
 	        for (var x = 0; x < 8; x++) {
 	            field[x] = new Array(8);
 	            for (var y = 0; y < 8; y++) {
-	                field[x][y] = Math.ceil(Math.random() * 7);
+	                field[x][y] = Math.ceil(Math.random() * 7)*10;
 	                self._addPiece(x, y, field[x][y]);
 	            }
 	        }
-	     self._processField();
+	        console.log(field);
+	    	self._processField();
         },
-
-
 
 	    _swap: function(elem1, elem2, cancelProcessing) {
 
@@ -94,12 +210,12 @@ define(['jquery'], function($) {
 	        }
 	        return false;
 	    },
-
+	    
 	    _processField: function(elem1, elem2) {
 
 	        var tmp = [];
 	        var x, y;
-
+	        var extraBonus = 0;
 	        for (x = 0; x < 8; x++) {
 	            tmp[x] = [];
 	            for (y = 0; y < 8; y++) {
@@ -109,43 +225,80 @@ define(['jquery'], function($) {
 
 	        for (x = 0; x < 8; x++) {
 	            for (y = 0; y < 8; y++) {
-	                if (y > 0 && y < 7 && field[x][y] == field[x][y - 1] && field[x][y] == field[x][y + 1]) {
-	                    tmp[x][y] = 1;
-	                    tmp[x][y - 1] = 1;
-	                    tmp[x][y + 1] = 1;
-	                }
-	                if (x > 0 && x < 7 && field[x][y] == field[x - 1][y] && field[x][y] == field[x + 1][y]) {
-	                    tmp[x][y] = 1;
-	                    tmp[x - 1][y] = 1;
-	                    tmp[x + 1][y] = 1;
-	                }
+	            	if(field[x][y]%10 == 1)//bomb
+	            	{
+	            		if(x>0 && y>0)  tmp[x-1][y-1] = 1;
+	            		if(x>0)			tmp[x-1][y] = 1;
+	            		if(x>0 && y<7)	tmp[x-1][y+1] = 1;
+	            		if(y>0)			tmp[x][y-1] = 1;
+	            						tmp[x][y] = 1;
+	            		if(y<7)			tmp[x][y+1] = 1;
+	            		if(x<7 && y>0)	tmp[x+1][y-1] = 1;
+	            		if(x<7)			tmp[x+1][y] = 1;
+	            		if(x<7 && y<7)	tmp[x+1][y+1] = 1;
+	            	}
+	                else
+	                {
+		               	if (y > 0 && y < 7 && parseInt(field[x][y]/10) == parseInt(field[x][y - 1]/10) && parseInt(field[x][y]/10) == parseInt(field[x][y + 1]/10)) {
+		                    tmp[x][y] = 1;
+		                    tmp[x][y - 1] = 1;
+		                    tmp[x][y + 1] = 1;
+		                    if(field[x][y]%10 == 2)//bonus
+	            				extraBonus += 3;
+	            			if(field[x][y-1]%10 == 2)//bonus
+	            				extraBonus += 3;
+	            			if(field[x][y+1]%10 == 2)//bonus
+	            				extraBonus += 3;
+		                }
+		                if (x > 0 && x < 7 && parseInt(field[x][y]/10) == parseInt(field[x - 1][y]/10) && parseInt(field[x][y]/10) == parseInt(field[x + 1][y]/10)) {
+		                    tmp[x][y] = 1;
+		                    tmp[x - 1][y] = 1;
+		                    tmp[x + 1][y] = 1;
+		                    if(field[x][y]%10 == 2)//bonus
+	            				extraBonus += 3;
+	            			if(field[x-1][y]%10 == 2)//bonus
+	            				extraBonus += 3;
+	            			if(field[x+1][y]%10 == 2)//bonus
+	            				extraBonus += 3;
+		                }
+		            }
 	            }
 	        }
 
 	        var deleted = 0;
-
+	        allDeleted = 0;
+	        var arr = [];
 	        for (x = 0; x < 8; x++) {
 	            for (y = 0; y < 8; y++) {
 	                if (tmp[x][y] == 1) {
+	                	var arr_tmp = [];
+	                	arr_tmp.push(x);
+	                	arr_tmp.push(y);
+	                	arr.push(arr_tmp);
+
 	                    field[x][y] = 0;
 	                    $(('#xy' + x) + y).addClass('remove');
 	                    deleted++;
 	                }
 	            }
 	        }
-
 	        $('.remove').animate({ opacity: 0 }, 500, function() {
 	            $('.remove').remove();
 	        });
 
 	        if (deleted > 0) {
-	            points += Math.min(Math.pow(2, deleted - 3), 20 * deleted);
-	            $('#points').html(points);
+	        	allDeleted = deleted;
+	            points += deleted + extraBonus;
+	            if(points>nLevel*500 && nGameMode == 3){
+	            	nLevel++;
+	            	points = 0;
+	            	$('#gameState').html("Untimed-><br>Level Mode<br>Level "+nLevel+" : "+nLevel*500);
+	            }
+	            $('#points').html('Your Score<br>'+points);
+	            setTimeout('self._caving()', 500);
 	        }
 
-	        if (deleted > 0) {
-	            setTimeout('self._caving()', 500);
-	        } else if (elem1 && elem2) {
+	        else if (elem1 && elem2) {
 	            self._swap(elem1, elem2, true);
 	        }
 	    },
@@ -170,13 +323,45 @@ define(['jquery'], function($) {
 	                }
 	                yfull--;
 	            }
+	            console.log(allDeleted);
+	            if(allDeleted == 5)
+	            {
+	            	type = Math.ceil(Math.random() * 7)*10+1;
+	                field[x][deleted - 1] = type;
+	                self._addPiece(x, deleted - 1, type, 1).animate({ top: '+=' + ((deleted + 2) * 64) }, 150 * (deleted + 2));
+	                delay = Math.max(delay, 150 * (deleted + 2));
 
-	            for (var i = 1; i <= deleted; i++) {
-	                type = Math.ceil(Math.random() * 7);
-	                field[x][deleted - i] = type;
-	                self._addPiece(x, deleted - i, type, i).animate({ top: '+=' + ((deleted + i + 1) * 64) }, 150 * (deleted + i + 1));
-	                delay = Math.max(delay, 150 * (deleted + i + 1));
+	            	for (var i = 2 ; i <= deleted; i++) {
+		                type = Math.ceil(Math.random() * 7)*10;
+		                field[x][deleted - i] = type;
+		                self._addPiece(x, deleted - i, type, i).animate({ top: '+=' + ((deleted + i + 1) * 64) }, 150 * (deleted + i + 1));
+		                delay = Math.max(delay, 150 * (deleted + i + 1));
+		            }
 	            }
+
+	            else if(allDeleted == 4)
+	            {
+	            	type = Math.ceil(Math.random() * 7)*10+2;
+	                field[x][deleted - 1] = type;
+	                self._addPiece(x, deleted - 1, type, 1).animate({ top: '+=' + ((deleted + 2) * 64) }, 150 * (deleted + 2));
+	                delay = Math.max(delay, 150 * (deleted + 2));
+
+	            	for (var i = 2 ; i <= deleted; i++) {
+		                type = Math.ceil(Math.random() * 7)*10;
+		                field[x][deleted - i] = type;
+		                self._addPiece(x, deleted - i, type, i).animate({ top: '+=' + ((deleted + i + 1) * 64) }, 150 * (deleted + i + 1));
+		                delay = Math.max(delay, 150 * (deleted + i + 1));
+		            }
+	            }
+
+	            else{
+	            	for (var i = 1 ; i <= deleted; i++) {
+		                type = Math.ceil(Math.random() * 7)*10;
+		                field[x][deleted - i] = type;
+		                self._addPiece(x, deleted - i, type, i).animate({ top: '+=' + ((deleted + i + 1) * 64) }, 150 * (deleted + i + 1));
+		                delay = Math.max(delay, 150 * (deleted + i + 1));
+		            }
+		        }
 	        }
 
 	        setTimeout('self._processField()', delay);
@@ -196,7 +381,6 @@ define(['jquery'], function($) {
 	            str += "\n";
 	        }
 	        console.log(str);
-	    }   	
-    	
+	    }
 	}
 });
--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,7 @@ requirejs.config({
 	paths: {
 		//The libraries we use:
 		jquery: [
-            //Try loading from CDN first:
-			'//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min',
-			//If it can't load from the CDN, it'll load the local version below:
+            //If it can't load from the CDN, it'll load the local version below:
 			'lib/jquery-3.2.1'
 		]
 	
@@ -19,9 +17,62 @@ requirejs.config({
 
 
 //Main
-require(['jquery', 'app'], function($, app){
+var timeRemain;
+
+function setState(ele_name,state)
+{
+	if(state == 'false')
+		document.getElementById(ele_name).style.display = 'none';
+	else if(state == 'true')
+		document.getElementById(ele_name).style.display = 'block';
+}
+
+function timeCounter(){
+	timeRemain--;
+	document.getElementById('gameState').innerHTML = "Timed Mode<br>Playing Time<br>"+timeRemain+"s";
+	if(timeRemain >= 0 ) setTimeout(timeCounter,1000);
+	else{
+		alert("Your Game Was Finished.");
+		document.location.replace('index.html');
+	}
+}
+
+nLevel = 1;
+document.getElementById('btnStart').onclick = function()
+{
+	var primaryMode = document.getElementById('time').checked;
+	var secondaryMode = document.getElementById('casual').checked;
+
+	document.getElementById('startGame').style.display = 'none';
+	document.getElementById('field').style.display = 'block';
+	document.getElementById('points').style.display = 'block';
+	document.getElementById('points').innerHTML = "Your Score<br>0";
+
+	document.getElementById('gameState').style.display = 'block';
+	document.getElementById('btnHint').style.display = 'block';
+	document.getElementById('btnShuttle').style.display = 'block';
+
+	if(primaryMode == true)
+	{
+		nGameMode = 1;
+		document.getElementById('gameState').innerHTML = "Timed Mode<br>Playing Time<br>0s";
+		timeRemain = 40;
+		timeCounter();
+	}
+	else if(primaryMode == false && secondaryMode == true)
+	{
+		nGameMode = 2;
+		document.getElementById('gameState').innerHTML = "Untimed-><br>Casual Mode";
+	}
+	else if(primaryMode == false && secondaryMode == false)
+	{
+		nGameMode = 3;
+		document.getElementById('gameState').innerHTML = "Untimed-><br>Level Mode<br>Level "+nLevel+" : "+nLevel*500;
+		document.getElementById('showProgress').style.display = 'block';
+		document.getElementById('complete').style.width = (nLevel-1)*10+'%';
+	}
+	require(['jquery', 'app'], function($, app){
        $('#testing').html('this is the main module');
        app.init();
-
-});
-
+	});
+}
